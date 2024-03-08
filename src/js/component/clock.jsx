ed8eec6940e7c688018afe8e745ee1065435e210
Carla <152617155+Cl1493@users.noreply.github.com>
2024-03-08 20:44:28 +0000

    borrado de props foto reloj y arreglos

--- src/js/component/clock.jsx
+++ src/js/component/clock.jsx
@@ -1,34 +1,37 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
-function Counter (props) {
-    let counter= 0;
-    setInterval(function(){
-        const six = Math.floor(counter/100000);
-        const five = Math.floor(counter/10000);
-        const four = Math.floor(counter/1000);
-        const three = Math.floor(counter/100);
-        const two = Math.floor(counter/10);
-        const one = Math.floor(counter/1);
-        counter ++;},1000);
-    return (
-        <div className="bigCounter">
-            <div className="reloj"><i class="fa-regular fa-clock"></i></div>
-            <div className="six">{props.digitSix % 10}</div>
-            <div className="five">{props.digitFive % 10}</div>
-            <div className="four">{props.digitFour % 10}</div>
-            <div className="three">{props.digitThree % 10}</div>
-            <div className="two">{props.digitTwo % 10}</div>
-            <div className="one">{props.digitOne % 10}</div>
-        </div>
-    );
+
+function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCounter((prevCounter) => prevCounter + 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const six = Math.floor((counter / 100000) % 10);
+  const five = Math.floor((counter / 10000) % 10);
+  const four = Math.floor((counter / 1000) % 10);
+  const three = Math.floor((counter / 100) % 10);
+  const two = Math.floor((counter / 10) % 10);
+  const one = Math.floor(counter % 10);
+
+  return (
+    <div className="bigCounter">
+      <div className="reloj">
+        <img src="../src/img/Flaticon.jpg">
+      <div className="six">{six}</div>
+      <div className="five">{five}</div>
+      <div className="four">{four}</div>
+      <div className="three">{three}</div>
+      <div className="two">{two}</div>
+      <div className="one">{one}</div>
+    </div>
+  );
 }
 
-Counter.propTypes = {
-    digitOne: PropTypes.number,
-    digitTwo: PropTypes.number,
-    digitThree: PropTypes.number,
-    digitFour: PropTypes.number,
-    digitFive: PropTypes.number,
-    digitSix: PropTypes.number
-};
+
 export default Counter;
--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,8 @@ import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
-import Counter from "./component/clock";
-
-
-
+//import your own components
+import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Counter />, document.querySelector("#app"));
+ReactDOM.render(<Home />, document.querySelector("#app"));
